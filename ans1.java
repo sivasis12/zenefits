255120da872156866ede6ef618d732b9bc446266
sivasis12 <38074734+sivasis12@users.noreply.github.com>
2018-04-05 19:41:36 +0530

    Add files via upload

---
+++ ans1.java
@@ -0,0 +1,43 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Tree
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    String str=sc.next();
+		    Node root=str.
+		    int l=str.length();
+		    for(int i=0;i<l;i++)
+		   {
+		       char ch=str.charAt(i);
+		       if(ch=='{')
+		       {
+		           
+		       }
+		   }
+		}
+		
+	}
+}
+class Node{
+    Node left;
+    Node right;
+    Node data;
+    public Node(int d)
+    {
+       data=d;
+       left=null;
+       right=null;
+       
+    }
+}
